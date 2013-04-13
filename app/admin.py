83278d4f12595241adc69a79f2019f832646788a
prikozhd <prikozhd@gmail.com>
2013-04-14 01:35:36 +0300

    1:30 - test magazine bookzone

---
+++ app/admin.py
@@ -0,0 +1,14 @@
+#encoding:utf-8
+from django.contrib import admin
+from models import *
+
+class AdminProducts(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title', )}
+
+admin.site.register(Products, AdminProducts)
+
+admin.site.register(Category)
+
+
+
+
