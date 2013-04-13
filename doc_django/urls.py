4af6fdcb0704ae2beda31e9e45d230e64cf75973
prikozhd <prikozhd@gmail.com>
2013-04-14 01:37:18 +0300

    1:30 - test magazine bookzone

---
+++ doc_django/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+from settings import MEDIA_ROOT
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^products/$', 'app.views.products',  {'all_category':''},name = 'products'),
+    url(r'^product/(?P<slug>[a-zA-Z0-9_-]*)/$', 'app.views.get_product', name = 'product'),
+
+    url(r'^admin_tools/', include('admin_tools.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':MEDIA_ROOT})
+)
+
+
