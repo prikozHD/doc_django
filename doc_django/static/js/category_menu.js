8295ea64b890e6672e1ee3a531b81c64b7657f89
prikozhd <prikozhd@gmail.com>
2013-04-14 03:09:46 +0300

    1:30 - test magazine bookzone

--- doc_django/static/js/category_menu.js
+++ doc_django/static/js/category_menu.js
@@ -1,6 +1,8 @@
 $(".main_category").click(function () {
+
     if($(this).children().css('display') == 'none'){
         $(this).children().slideDown();
+
     }
     else{
         $(this).children().slideUp();
