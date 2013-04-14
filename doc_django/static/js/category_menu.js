0b028a7fe3281bfa03f0e89ba7e57d98d5f33fd4
prikozhd <prikozhd@gmail.com>
2013-04-14 03:08:34 +0300

    1:30 - test magazine bookzone

--- doc_django/static/js/category_menu.js
+++ doc_django/static/js/category_menu.js
@@ -1,10 +1,8 @@
 $(".main_category").click(function () {
-
-    console.log(this.getElementsByTagName('p'));
-//    if ($(".main_category).('..sub_category")').css('display') == 'none'){
-//        $(".main_category.sub_category").slideDown();
-//    }
-//    else{
-//        $(".main_category.sub_category").slideUp();
-//    }
+    if($(this).children().css('display') == 'none'){
+        $(this).children().slideDown();
+    }
+    else{
+        $(this).children().slideUp();
+    }
 });
